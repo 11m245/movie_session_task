4be7aa78ca312605780b0739bdd5481025a09b76
11m245 <sivaraj2siva@gmail.com>
2022-12-02 13:28:06 +0530

    update theme

--- src/App.js
+++ src/App.js
@@ -1,43 +1,72 @@
-
-import './App.css';
-import { useState } from 'react';
-import { MovieList } from './MovieList';
-import { AddMovieFn } from './AddMovieFn';
-import { Home } from './Home';
-import { NotFound } from './NotFound';
+import "./App.css";
+import { useState } from "react";
+import { MovieList } from "./MovieList";
+import { AddMovieFn } from "./AddMovieFn";
+import { Home } from "./Home";
+import { NotFound } from "./NotFound";
 import { Routes, Route, Link, Navigate } from "react-router-dom";
-import { ButtonAppBar } from "./ButtonAppBar.js"
-import { MovieDetails } from './MovieDetails';
-import { AddColor } from './AddColor';
-
+import { ButtonAppBar } from "./ButtonAppBar.js";
+import { MovieDetails } from "./MovieDetails";
+import { AddColor } from "./AddColor";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Paper from '@mui/material/Paper';
 
 function App() {
   const initial_movie_list = [{ "id": "99", "name": "Vikram", "poster": "https://m.media-amazon.com/images/M/MV5BMmJhYTYxMGEtNjQ5NS00MWZiLWEwN2ItYjJmMWE2YTU1YWYxXkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg", "rating": 8.4, "summary": "Members of a black ops team must track and eliminate a gang of masked murderers.", "trailer": "https://www.youtube.com/embed/OKBMCL-frPU" }, { "id": "100", "name": "RRR", "poster": "https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/6/Desk/2021_6$largeimg_977224513.JPG", "rating": 8.8, "summary": "RRR is an upcoming Indian Telugu-language period action drama film directed by S. S. Rajamouli, and produced by D. V. V. Danayya of DVV Entertainments.", "trailer": "https://www.youtube.com/embed/f_vbAtFSEc0" }, { "id": "101", "name": "Iron man 2", "poster": "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg", "rating": 7, "summary": "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.", "trailer": "https://www.youtube.com/embed/wKtcmiifycU" }, { "id": "102", "name": "No Country for Old Men", "poster": "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg", "rating": 8.1, "summary": "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.", "trailer": "https://www.youtube.com/embed/38A__WT3-o0" }, { "id": "103", "name": "Jai Bhim", "poster": "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg", "summary": "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case", "rating": 8.8, "trailer": "https://www.youtube.com/embed/nnXpbTFrqXA" }, { "id": "104", "name": "The Avengers", "rating": 8, "summary": "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.", "poster": "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg", "trailer": "https://www.youtube.com/embed/eOrNdBpGMv8" }, { "id": "105", "name": "Interstellar", "poster": "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg", "rating": 8.6, "summary": "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.", "trailer": "https://www.youtube.com/embed/zSWdZVtXT7E" }, { "id": "106", "name": "Baahubali", "poster": "https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg", "rating": 8, "summary": "In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.", "trailer": "https://www.youtube.com/embed/sOEg_YZQsTI" }, { "id": "107", "name": "Ratatouille", "poster": "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=", "rating": 8, "summary": "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.", "trailer": "https://www.youtube.com/embed/NgsQ8mVkN8w" }];
 
   const [movieList, setmovieList] = useState(initial_movie_list);
 
-  return (
-    <div className="Movie-App">
-      <ButtonAppBar />
-      <ul className="nav-items-wrapper">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/movies">Movies</Link></li>
-        <li><Link to="/add-movie">Add Movie</Link></li>
-        <li><Link to="/color-game">Color Game</Link></li>
-      </ul>
-      <h1 style={{ textAlign: "center" }}>Welcome to Movie App ⭐</h1>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/add-movie' element={<AddMovieFn movieList={movieList} setmovieList={setmovieList} />} />
-        <Route path='/movies' element={<MovieList movieList={movieList} />} />
-        <Route path="/film" element={<MovieList movieList={movieList} />} />
-        <Route path="/films" element={<Navigate replace to="/movies" />} />
-        <Route path="/movies/:id" element={<MovieDetails movieList={movieList} />} />
-        <Route path='*' element={<NotFound />} />
-        <Route path='/color-game' element={<AddColor />} />
-      </Routes>
+  const [mode, setMode] = useState("light");
 
-    </div>
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+
+
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Paper elevation={8} >
+        <div className="Movie-App">
+          <ButtonAppBar mode={mode} setMode={setMode} />
+          <ul className="nav-items-wrapper">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/movies">Movies</Link>
+            </li>
+            <li>
+              <Link to="/add-movie">Add Movie</Link>
+            </li>
+            <li>
+              <Link to="/color-game">Color Game</Link>
+            </li>
+          </ul>
+          <h1 style={{ textAlign: "center" }}>Welcome to Movie App ⭐</h1>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/add-movie"
+              element={
+                <AddMovieFn movieList={movieList} setmovieList={setmovieList} />
+              }
+            />
+            <Route path="/movies" element={<MovieList movieList={movieList} />} />
+            <Route path="/film" element={<MovieList movieList={movieList} />} />
+            <Route path="/films" element={<Navigate replace to="/movies" />} />
+            <Route
+              path="/movies/:id"
+              element={<MovieDetails movieList={movieList} />}
+            />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/color-game" element={<AddColor />} />
+          </Routes>
+        </div>
+      </Paper >
+    </ThemeProvider>
   );
 }
 
--- src/ButtonAppBar.js
+++ src/ButtonAppBar.js
@@ -4,9 +4,12 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 
 
-function ButtonAppBar() {
+
+function ButtonAppBar({ mode, setMode }) {
 
     const navigate = useNavigate();
     return (
@@ -17,6 +20,8 @@ function ButtonAppBar() {
                     <Button onClick={() => navigate("/movies")} color="inherit">Movies</Button>
                     <Button onClick={() => navigate("/add-movie")} color="inherit">Add Movie</Button>
                     <Button onClick={() => navigate("/color-game")} color="inherit">Color Game</Button>
+                    <Button onClick={() => setMode(mode === "light" ? "dark" : "light")} color="inherit">
+                        {mode === "light" ? "dark" : "light"} mode    {mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}</Button>
                 </Toolbar>
             </AppBar>
         </Box>
